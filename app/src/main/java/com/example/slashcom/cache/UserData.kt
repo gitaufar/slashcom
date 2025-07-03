fd3920c543778207a14f1f6fe8b129185612c676
gitaufar <zhafiraufar123@gmail.com>
2025-07-03 17:07:23 +0700

    feat: integration dashboard screen to firebase

--- app/src/main/java/com/example/slashcom/cache/UserData.kt
+++ app/src/main/java/com/example/slashcom/cache/UserData.kt
@@ -1,7 +1,11 @@
 package com.example.slashcom.cache
 
+import com.example.slashcom.domain.model.Mood
+
 object UserData {
     var userName: String = ""
     var uid: String = ""
     var isIbu: Boolean = false
+    var lastMood: Mood? = null
+    var listMoods: List<Mood>? = null
 }
